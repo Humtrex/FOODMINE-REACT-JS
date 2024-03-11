47a599497f53deeb350a90ad9b3bc2f946865e74
Humtrex <humberto_hdezt@hotmail.com>
2024-03-11 17:23:01 -0600

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import AppRoutes from "./AppRoutes";
+import Header from "./components/Header/Header";
+
+function App() {
+  return <>
+  <Header/>
+  <AppRoutes/>
+  </>;
+}
+
+export default App;
