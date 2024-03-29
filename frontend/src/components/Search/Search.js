b45600780f97c05d9b0812b03b6f8c1e87ad096b
Humtrex <humberto_hdezt@hotmail.com>
2024-03-28 22:30:26 -0600

    Foods Admin Page

--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import classes from './search.module.css';
 
-export default function Search() {
+Search.defaultProps = {
+    searchRoute: '/search/',
+    defaultRoute: '/',
+};
+
+export default function Search({ searchRoute, defaultRoute, margin}) {
     const [term, setTerm] = useState('');
     const navigate = useNavigate();
     const {searchTerm} = useParams();
@@ -12,13 +17,13 @@ export default function Search() {
     }, [searchTerm]);
     
     const search = async ()=>{
-        term ? navigate('/search/'+ term) : navigate('');
+        term ? navigate(searchRoute + term) : navigate(defaultRoute);
     };
-    return <div className={classes.container}>
+    return <div className={classes.container} style={{margin}}>
         <input type='text'
         placeholder='Search Food Mine!'
         onChange={e => setTerm(e.target.value)}
-        onnKeyUp={e => e.key === 'Enter' && search()}
+        onKeyUp={e => e.key === 'Enter' && search()}
         value={term}
         />
         <button onClick={search}>Search</button>
